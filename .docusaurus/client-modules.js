47a38586f7118af7b0a1ec3797b0b7110cfeeed8
Eduardo Simões <123998309+eduardo895@users.noreply.github.com>
2025-11-27 14:46:05 +0000

    Inicializar o repo com o Docusaurus

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,7 @@
+export default [
+  require("/workspaces/Loja-Checkpoint-Site/.docusaurus/docusaurus-plugin-css-cascade-layers/default/layers.css"),
+  require("/workspaces/Loja-Checkpoint-Site/node_modules/infima/dist/css/default/default.css"),
+  require("/workspaces/Loja-Checkpoint-Site/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/workspaces/Loja-Checkpoint-Site/node_modules/@docusaurus/theme-classic/lib/nprogress"),
+  require("/workspaces/Loja-Checkpoint-Site/src/css/custom.css"),
+];
